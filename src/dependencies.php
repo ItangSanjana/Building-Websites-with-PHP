eece06323aaa5caf4b6869610ebca07eb2e469bd
Itang Sanjana <arwata@gmail.com>
2017-03-10 05:23:05 +0700

    src modify

--- src/dependencies.php
+++ src/dependencies.php
@@ -3,17 +3,29 @@
 
 $container = $app->getContainer();
 
-// view renderer
+// View renderer
 $container['renderer'] = function ($c) {
     $settings = $c->get('settings')['renderer'];
     return new Slim\Views\PhpRenderer($settings['template_path']);
 };
 
-// monolog
+// Monolog
 $container['logger'] = function ($c) {
     $settings = $c->get('settings')['logger'];
     $logger = new Monolog\Logger($settings['name']);
     $logger->pushProcessor(new Monolog\Processor\UidProcessor());
     $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
     return $logger;
+};
+
+// Messager
+$container['messager'] = function ($c) {
+    $settings = $c->get('settings')['messager'];
+    return Swift_Message::newInstance($settings['subject']);
+};
+
+// Mailer
+$container['mailer'] = function ($c) {
+    $settings = $c->get('settings')['mailer'];
+    return Swift_Mailer::newInstance($settings['transport']);
 };
